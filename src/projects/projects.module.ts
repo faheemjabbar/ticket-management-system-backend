2af7db7b474cf423decfbbd3b5a0ecb962ac6df6
faheemjabbar <faheemjabbar36@gmail.com>
2026-02-12 17:26:49 +0500

    feat: add Organization and User models to projects module

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 import { Project, ProjectSchema } from './schemas/project.schema';
+import { User, UserSchema } from '../users/schemas/user.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Project.name, schema: ProjectSchema }]),
+    MongooseModule.forFeature([
+      { name: Project.name, schema: ProjectSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
   ],
   controllers: [ProjectsController],
   providers: [ProjectsService],
